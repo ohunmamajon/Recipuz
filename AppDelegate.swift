5abfec09f7c2eff9a24493bd12de36988aaa774e
ohunmamajon <97847786+ohunmamajon@users.noreply.github.com>
2023-02-07 10:08:04 +0900

    launch screen

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -14,11 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        UIFont.familyNames.forEach({name in
-            for name in UIFont.fontNames(forFamilyName: name){
-                print ("\(name)\n")
-            }
-        })
         
         return true
     }
